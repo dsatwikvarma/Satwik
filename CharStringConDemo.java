794d14202c587c221244bad0dcbe66de59231ea5
dsatwikvarma <datlasatwik@gmail.com>
2016-07-19 21:31:33 +0530

    Add files via upload

---
+++ CharStringConDemo.java
@@ -0,0 +1,14 @@
+// Char & String Demo
+import java.io.*;
+class CharStringConDemo 
+{
+	public static void main(String[] args) 
+	{
+		char ch='A';
+		char ch1=65;//ASCII value for capital 'A'
+		String s1="core java";
+		System.out.println("char value="+ch);
+		System.out.println("char value="+ch1);
+		System.out.println("string value="+s1);
+	}
+}
